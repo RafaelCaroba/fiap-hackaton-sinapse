49fb4f3771aebeabacc4f28f4ccde2dd7e905aad
Rafael Caroba de Souza <rafa.caroba.17@gmail.com>
2026-07-21 02:21:06 -0300

    feature/pacient-entity -> Criação do modelo de Paciente

---
+++ patient-service/src/main/java/br/com/sinapse/patient/domain/Gender.java
@@ -0,0 +1,7 @@
+package br.com.sinapse.patient.domain;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ patient-service/src/main/java/br/com/sinapse/patient/domain/Patient.java
@@ -0,0 +1,48 @@
+package br.com.sinapse.patient.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "patients", schema = "patient")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BloodType bloodType;
+}
